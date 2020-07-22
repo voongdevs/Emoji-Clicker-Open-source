dc29026153ee62fd8939c68cf1fc1031bdf63140
Peter Voong <peterstar.voo@gmail.com>
2020-07-22 13:11:09 -0700

    added click

--- game.js
+++ game.js
@@ -1,11 +1,16 @@
 alert("hi")
 let money = 0
-function init(){
+function init(){ 
+    document.querySelector(".grin-icon").addEventListener("click", clickEmoji)
     
 }
 function clickEmoji(){
     money++
+    render()
 }
 function render(){
+    document.querySelector(".counter-value").innerText = money
     
 }
+
+init()
